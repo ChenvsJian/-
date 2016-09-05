74347b1fd0460e9d0e70bc15497917f32f255b8e
CJ <chenjian8156@126.com>
2016-09-05 15:41:16 +0800

    1

---
+++ js/ajax.php
@@ -0,0 +1,13 @@
+<?php
+
+    header("Content-Type: application/json; charset=utf-8");
+    echo $_GET["username"];
+    // echo $_GET["userphone"];
+    // echo $_GET["usertext"];
+// 
+
+
+    // echo 'hello';
+
+
+?>
